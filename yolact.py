552b329d78936ce3f8d25a84d3cde21410a21cca
BESBES Mohamed Dhia Elhak <mohmed-dhia-elhak.besbes@altran.com>
2022-06-26 22:13:52 +0200

    ff

--- yolact.py
+++ yolact.py
@@ -282,7 +282,7 @@ class GCN(MessagePassing):
         self.bias.data.zero_()
     def forward(self, node_feats):
         x = torch.tensor_split(node_feats[0], self.nbr_nodes, dim=0)
-        print(x.shape)
+        print(adj_matrix)
         edge_index = self.adj_matrix.nonzero().t().contiguous()
         edge_index, _ = add_self_loops(edge_index, num_nodes=self.nbr_nodes)
         x = torch.stack(x)
