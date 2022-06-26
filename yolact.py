ff254dda74640ef1f4e6229d045622206ba2a5ae
BESBES Mohamed Dhia Elhak <mohmed-dhia-elhak.besbes@altran.com>
2022-06-26 23:11:28 +0200

    ff

--- yolact.py
+++ yolact.py
@@ -631,7 +631,9 @@ class Yolact(nn.Module):
             with timer.env('fpn'):
                 # Use backbone.selected_layers because we overwrote self.selected_layers
                 outs = [outs[i] for i in cfg.backbone.selected_layers]
+                print(len(outs))
                 outs = self.gcns(outs)
+                print(len(outs))
                 outs = self.fpn(outs)
 
 
