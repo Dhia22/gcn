d42fcef4270fa17f27d3c0164c069613600897f0
BESBES Mohamed Dhia Elhak <mohmed-dhia-elhak.besbes@altran.com>
2022-06-26 22:41:54 +0200

    ff

--- yolact.py
+++ yolact.py
@@ -273,6 +273,8 @@ class GCN(MessagePassing):
             self.adj_matrix = np.ones((self.nbr_nodes, self.nbr_nodes))
             np.fill_diagonal(self.adj_matrix, 0)
             self.adj_matrix = torch.tensor(self.adj_matrix).cuda()
+        self.edge_index = self.adj_matrix.nonzero().t().contiguous()
+        self.edge_index, _ = add_self_loops(edge_index, num_nodes=self.nbr_nodes)
         self.lin = Linear(in_channels, out_channels, bias=False)
         self.bias = Parameter(torch.Tensor(out_channels))
         self.reset_parameters()
@@ -282,8 +284,6 @@ class GCN(MessagePassing):
         self.bias.data.zero_()
     def forward(self, node_feats):
         x = torch.tensor_split(node_feats[0], self.nbr_nodes, dim=0)
-        self.edge_index = self.adj_matrix.nonzero().t().contiguous()
-        self.edge_index, _ = add_self_loops(edge_index, num_nodes=self.nbr_nodes)
         x = torch.stack(x)
         x = self.lin(x)
         row, col = self.edge_index
