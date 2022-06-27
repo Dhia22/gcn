8c43856ab736d14d8faf99157fbc27ff678b33ef
BESBES Mohamed Dhia Elhak <mohmed-dhia-elhak.besbes@altran.com>
2022-06-27 10:28:18 +0200

    ff

--- yolact.py
+++ yolact.py
@@ -316,7 +316,8 @@ class GCN(MessagePassing):
         norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
         out = self.propagate(edge_index, x=x, norm=norm)
         out += self.bias
-        out = out.reshape(out.shape[0]*out.shape[1],out.shape[2],out.shape[3])
+        print(out.shape)
+        #out = out.reshape(out.shape[0]*out.shape[1],out.shape[2],out.shape[3])
         return out[None, :]
 
     def message(self, x_j, norm):
